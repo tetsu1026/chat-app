447b976ad00745413ad5e4257bbe542833f3b58c
tetsu1026 <tetesu.w1026@gmail.com>
2020-12-25 15:21:51 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -57,3 +57,6 @@ gem 'devise'
 
 gem 'pry-rails'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   validates :content, presence: true
-  
+  has_one_attached :image
   belongs_to :room
   belongs_to :user
 end
